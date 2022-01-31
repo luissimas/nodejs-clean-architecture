b8928564ee985239b5c91c7eed9ef958c42943f8
Luís Simas <luissimas@protonmail.com>
2022-01-31 07:09:50 -0300

    Dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import { router } from './routes'
 
+// Loading dotenv config
+import 'dotenv/config'
+
 const app = express()
 
 app.use(express.json())
--- src/providers/Mail/implementations/MailtrapMailProvider.ts
+++ src/providers/Mail/implementations/MailtrapMailProvider.ts
@@ -1,17 +1,17 @@
 import { IMailProvider, IMessage } from '../IMailProvider'
 import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
-
+import 'dotenv/config'
 export class MailtrapMailProvider implements IMailProvider {
   private transporter: Mail
 
   constructor () {
     this.transporter = nodemailer.createTransport({
-      host: 'smtp.mailtrap.io',
-      port: 2525,
+      host: process.env.MAILTRAP_HOST,
+      port: Number(process.env.MAILTRAP_PORT),
       auth: {
-        user: '88e7722011ba81',
-        pass: '121f5f780a41bb'
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS
       }
     })
   }
